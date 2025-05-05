f79327a0cad0f4a5a8dd89b1aca5fe3ea0edbf01
Shay Moreno <moa129@gmail.com>
2025-05-05 15:15:44 +0300

    Initial Commit

---
+++ VerbaX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VerbaX
+//
+//  Created by shay moreno on 05/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VerbaX/VerbaXApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerbaXApp.swift
+//  VerbaX
+//
+//  Created by shay moreno on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct VerbaXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
